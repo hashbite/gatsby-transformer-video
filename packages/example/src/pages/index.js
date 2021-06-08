ee1a92064ec1d113a95cf550e251339f0ae0d23d
axe312ger <opensource@axe312.dev>
2021-06-08 11:48:15 +0200

    docs(example): reduce to one video, add 0s screenshot and VP9

--- packages/example/src/pages/index.js
+++ packages/example/src/pages/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react";
+import { graphql } from "gatsby";
 
-import './styles.css'
+import "./styles.css";
 
 function Video({
   video: {
     name,
     videoH264,
     videoH265,
+    videoVP9,
     previewH264,
     previewWebP,
     previewGif,
@@ -18,8 +19,10 @@ function Video({
 }) {
   return (
     <div {...props}>
-      <h2>Example: {name.substring(0, 1).toUpperCase() + name.substring(1)}</h2>
-      <h3>Screenshots at 1s, 50% and 99%:</h3>
+      <h2>
+        Example Video: {name.substring(0, 1).toUpperCase() + name.substring(1)}
+      </h2>
+      <h3>Screenshots at 0s, 1s, 50% and 99%:</h3>
       <div className="screenshots">
         {videoScreenshots.map(({ publicURL }) => (
           <div key="path">
@@ -34,12 +37,13 @@ function Video({
         </a>
         :
       </h3>
+      <p>Fallbacks: h264, animated webP and Gifs</p>
       <picture>
         <source type="video/mp4" srcSet={previewH264.path} />
         <source type="image/webp" srcSet={previewWebP.path} />
         <img loading="lazy" src={previewGif.path} alt="" />
       </picture>
-      <h3>Video as optimized h264 &amp; h265:</h3>
+      <h3>Video as optimized h264 &amp; h265 &amp; VP9:</h3>
       <video
         playsInline
         preload="auto"
@@ -47,6 +51,7 @@ function Video({
         controls
       >
         <source src={videoH265.path} type="video/mp4; codecs=hevc" />
+        <source src={videoVP9.path} type="video/webm; codecs=vp9,opus" />
         <source src={videoH264.path} type="video/mp4; codecs=avc1" />
       </video>
       <h3>Custom video converter:</h3>
@@ -59,25 +64,25 @@ function Video({
         <source src={videoSepia.path} type="video/mp4; codecs=avc1" />
       </video>
     </div>
-  )
+  );
 }
 
 const Index = ({ data }) => {
-  const videos = data.allFile.edges.map(({ node }) => node)
+  const videos = data.allFile.edges.map(({ node }) => node);
 
   return (
-    <div>
+    <main>
       <h1>Using gatsby-transformer-video</h1>
       <div className="grid">
         {videos.map((video) => (
           <Video key={video.id} video={video} />
         ))}
       </div>
-    </div>
-  )
-}
+    </main>
+  );
+};
 
-export default Index
+export default Index;
 
 export const query = graphql`
   query HomePageQuery {
@@ -86,23 +91,16 @@ export const query = graphql`
         node {
           id
           name
-          videoH264(
-            overlay: "gatsby.png"
-            overlayX: "end"
-            overlayY: "start"
-            overlayPadding: 25
-          ) {
+          videoH264 {
             path
           }
-          videoH265(
-            overlay: "gatsby.png"
-            overlayX: "end"
-            overlayY: "start"
-            overlayPadding: 25
-          ) {
+          videoH265 {
             path
           }
-          videoSepia: videoProfile(profile: "sepia", maxWidth: 800) {
+          videoVP9 {
+            path
+          }
+          videoSepia: videoProfile(profile: "sepia", maxWidth: 600) {
             path
           }
           previewH264: videoH264(maxWidth: 600, fps: 4, duration: 2) {
@@ -111,14 +109,14 @@ export const query = graphql`
           previewWebP: videoWebP(maxWidth: 600, fps: 4, duration: 2) {
             path
           }
-          previewGif: videoGif(maxWidth: 300, fps: 4, duration: 2) {
+          previewGif: videoGif(maxWidth: 600, fps: 4, duration: 2) {
             path
           }
-          videoScreenshots(timestamps: ["1", "50%", "99%"]) {
+          videoScreenshots(timestamps: ["0", "1", "50%", "99%"]) {
             publicURL
           }
         }
       }
     }
   }
-`
+`;
